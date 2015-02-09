c4fc5a1499bda6f802ea2deaa184bd3f1aef67c0
KCh <kch@list.ru>
2015-02-09 16:06:33 +0300

    Fix image_uploader

--- app/uploaders/rhinocatalog/image_uploader.rb
+++ app/uploaders/rhinocatalog/image_uploader.rb
@@ -23,19 +23,15 @@ module Rhinocatalog
 			"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
 		end
 
-		# process :resize_to_limit => [736, nil]  
+		process :resize_to_limit => [736, nil]  
 
-		# version :large do
-		# 	process :resize_to_limit => [736, nil]  
-		# end
+		version :large do
+			process :resize_to_limit => [736, nil]  
+		end
 
-		# version :big_thumb do
-		# 	process resize_to_fill: [364, 189]
-		# end
-
-		# version :thumb do
-		# 	process resize_to_fill: [120, 120]
-		# end
+		version :thumb do
+			process resize_to_fill: [120, 120]
+		end
 
 		# Add a white list of extensions which are allowed to be uploaded.
 		# For images you might use something like this:

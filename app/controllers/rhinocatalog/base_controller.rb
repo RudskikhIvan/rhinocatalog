df4959722513ca4a15632191024108c0e661c016
KCh <kch@list.ru>
2015-02-09 19:19:27 +0300

    Fix Rhinocatalog BaseController authorize

--- app/controllers/rhinocatalog/base_controller.rb
+++ app/controllers/rhinocatalog/base_controller.rb
@@ -3,7 +3,7 @@ require_dependency "rhinocatalog/application_controller"
 module Rhinocatalog
 	class BaseController < Rhinoart::BaseController
 		before_action :set_locale
-		before_action { authorize! :manage, :books }
+		before_action { authorize! :manage, :catalog }
 
 		def default_url_options(options={})
 			if I18n.locale != I18n.default_locale

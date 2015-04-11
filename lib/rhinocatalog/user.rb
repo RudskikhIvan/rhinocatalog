d35cebda6e5ba20a996514c3cf4d9e601152d406
KCh <kch@list.ru>
2015-04-11 21:22:25 +0300

    Make api roles throw rolefy

--- lib/rhinocatalog/user.rb
+++ lib/rhinocatalog/user.rb
@@ -1,18 +1,18 @@
 module Rhinocatalog
 	module User
-		extend ActiveSupport::Concern
+		extend ActiveSupport::Concern		
 
 		included do
 			before_validation :set_api_token
+			before_save :update_api_roles
+			
+			attr_accessor :api_roles, :api_roles_changed
 
 			Rhinoart::User::ADMIN_PANEL_ROLE_CATALOG_MANAGER = "Catalog Manager"
 			Rhinoart::User::ADMIN_PANEL_ROLES << Rhinoart::User::ADMIN_PANEL_ROLE_CATALOG_MANAGER
 
 			Rhinoart::User::API_ROLE_USER_MOBILE_APP = "Mobile Application User"
 			Rhinoart::User::API_ROLES = [Rhinoart::User::API_ROLE_USER_MOBILE_APP]
-
-			Rhinoart::User::SAFE_INFO_ACCESSORS = [:street, :city, :state, :zip, :interest_level]
-			Rhinoart::User::store :info, accessors: Rhinoart::User::SAFE_INFO_ACCESSORS, coder: JSON    
 		end
 
 		def has_access_to_api?
@@ -28,5 +28,15 @@ module Rhinocatalog
 					self.api_token = SecureRandom.hex(17)
 				end
 			end
+
+	        def update_api_roles
+	        	clear_roles Rhinoart::User::API_ROLES if api_roles_changed.present? && api_roles_changed
+
+	            if api_roles.present? && api_roles.any?	                
+	                api_roles.each do |r| 
+	                    self.add_role r 
+	                end 
+	            end
+	        end
 	end
 end
